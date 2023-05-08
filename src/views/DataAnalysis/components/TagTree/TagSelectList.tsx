88390ec96d7382819fa593c737cf115d154c4af6
mirajo <mira.jo.kr@gmail.com>
2023-05-08 18:44:24 +0900

    feat(Data Analysis): Change from Korean to Japanese

--- src/views/DataAnalysis/components/TagTree/TagSelectList.tsx
+++ src/views/DataAnalysis/components/TagTree/TagSelectList.tsx
@@ -147,7 +147,7 @@ const TagSelectList = (props: any) => {
         {tagName.length > 0 && (
           <div>
             <Text fontSize="md" color="secondaryGray.600" fontWeight="700" mb="5px" marginLeft={2}>
-              선택된 태그
+              選択したTag
             </Text>
             <Text fontSize="md" color="secondaryGray.600" fontWeight="300" mb="5px" marginLeft={2}>
               {tagName.join(', ')}
