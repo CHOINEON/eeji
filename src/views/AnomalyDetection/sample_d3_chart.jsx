326b3da045460169dbce8d840290c63eb75dda04
Haejin-ineeji <haejin@ineeji.com>
2023-09-08 16:49:43 +0900

    Annotation Cleanup

--- src/views/AnomalyDetection/sample_d3_chart.jsx
+++ src/views/AnomalyDetection/sample_d3_chart.jsx
@@ -1,108 +1,25 @@
 /* eslint-disable prettier/prettier */
-// import * as d3 from 'd3';
-
-// // Sample data: time vs temperature
-// const data = [
-//   { time: new Date('2023-07-01T00:00:00'), temperature: 20 },
-//   { time: new Date('2023-07-02T00:00:00'), temperature: 22 },
-//   { time: new Date('2023-07-03T00:00:00'), temperature: 25 },
-//   // Add more data points...
-// ];
-
-// // Create SVG dimensions
-// const width = 600;
-// const height = 400;
-// const margin = { top: 20, right: 20, bottom: 30, left: 40 };
-
-// // Create SVG container
-// const svg = d3.select('body')
-//   .append('svg')
-//   .attr('width', width)
-//   .attr('height', height);
-
-// // Create scales
-// const xScale = d3.scaleTime()
-//   .domain(d3.extent(data, d => d.time))
-//   .range([margin.left, width - margin.right]);
-
-// const yScale = d3.scaleLinear()
-//   .domain([0, d3.max(data, d => d.temperature)])
-//   .nice()
-//   .range([height - margin.bottom, margin.top]);
-
-// // Create line generator
-// const line = d3.line<{ time: Date; temperature: number }>()
-//   .x(d => xScale(d.time))
-//   .y(d => yScale(d.temperature));
-
-// // Add line
-// svg.append('path')
-//   .datum(data)
-//   .attr('fill', 'none')
-//   .attr('stroke', 'steelblue')
-//   .attr('stroke-width', 2)
-//   .attr('d', line);
-
-// // Add threshold line
-// const thresholdTemperature = 24; // Example threshold value
-// svg.append('line')
-//   .attr('x1', margin.left)
-//   .attr('x2', width - margin.right)
-//   .attr('y1', yScale(thresholdTemperature))
-//   .attr('y2', yScale(thresholdTemperature))
-//   .attr('stroke', 'red')
-//   .attr('stroke-width', 2)
-//   .attr('stroke-dasharray', '4 4');
-
-// // Add x and y axes
-// svg.append('g')
-//   .attr('transform', `translate(0,${height - margin.bottom})`)
-//   .call(d3.axisBottom(xScale));
-
-// svg.append('g')
-//   .attr('transform', `translate(${margin.left},0)`)
-//   .call(d3.axisLeft(yScale));
-
-// // Add labels
-// svg.append('text')
-//   .attr('x', width / 2)
-//   .attr('y', height - margin.bottom / 2)
-//   .attr('text-anchor', 'middle')
-//   .text('Time');
-
-// svg.append('text')
-//   .attr('transform', 'rotate(-90)')
-//   .attr('x', -height / 2)
-//   .attr('y', margin.left / 2)
-//   .attr('text-anchor', 'middle')
-//   .text('Temperature (°C)');
-
-//   export default sampled3chart
 import { Box } from '@chakra-ui/react'
 import React, { useState, useEffect } from 'react'
 import Plot from 'react-plotly.js'
 import { Switch, Space } from 'antd'
 
 const AdvancedChart = () => {
-  /*socket url*/
+  //새로운 주소로 바뀔 예정
   const originURL = 'ws://222.121.66.49:8000/ws/web'
 
   /* managing the states of incoming data */
   const [socketData, setSocketData] = useState({})
   const [chartData, setChartData] = useState([])
-  // const [anomalyScore, setAnomalyScore] = useState([])
-  const [threshold, setThreshold] = useState([])
   const [index, setIndex] = useState([])
-  const [isAnomaly, setIsAnomaly] = useState([])
+  // const [isAnomaly, setIsAnomaly] = useState([])
 
-  /*accumulating updated data into an array for storage.Soon these will be used as a plotly data*/
   const [dataArr, setDataArr] = useState([])
   const [testData, setTestData] = useState([])
   const [anomalyScoreArr, setAnomalyScoreArr] = useState([])
   const [thresholdArr, setThresholdArr] = useState([])
   // const [isAnomalyArr, setIsAnomalyArr] = useState([])
 
-  //whenever socketData comes in, keep updates
 
   useEffect(() => {
     if (socketData.data) {
@@ -121,7 +38,6 @@ const AdvancedChart = () => {
         return [...prev, socketData.thr]
       })
 
-      //for plotly chart
       const plotData = [
         {
           x: index,
@@ -132,9 +48,7 @@ const AdvancedChart = () => {
           line: {
             color: 'black',
             width: '2',
-          },
-          // marker: isAnomaly === false ? { color: 'red' } : { color: 'blue' },
-        },
+          }},
         {
           x: index,
           y: anomalyScoreArr,
@@ -159,30 +73,12 @@ const AdvancedChart = () => {
           yaxis: 'y2',
           visible : yesVisible,
         },
-        //   type: 'dot',
-        //   mode: 'markers',
-        //   // marker: isAnomaly === false ? { color: 'red' } : { color: 'blue' },
-        //   marker: { color: colors, size: 8 },
-        // },
       ]
       setTestData(plotData)
     }
-    // setAnomalyScore(socketData.anomaly_pred)
-    // setThreshold(socketData.thr)
-    // setIsAnomaly(socketData.is_anormaly)
-    // setIndex([...index, socketData.index])
-    // setIndex(socketData.index)
-  }, [socketData])
+}, [socketData])
 
-  /*store data into array by using spread operator*/
-  // useEffect(() => {
-  //   //  setDataArr((prev) => [ ...prev,chartData])
-  //   setAnomalyScoreArr((prev) => [...prev, anomalyScore ])
-  //   setThresholdArr((prev)=> [...prev,threshold])
-  //   setIsAnomalyArr((prev) => [...prev,isAnomaly ])
-  // }, [socketData])
-
-  /*add timestamp to the end of the url*/
+  /*add timestamp*/
   useEffect(() => {
     var timestamp = Date.now()
     var newURL = `${originURL}${timestamp}`
@@ -198,7 +94,6 @@ const AdvancedChart = () => {
       try {
         const dataObj = JSON.parse(dataString)
         setSocketData(dataObj)
-        // console.log('dataOjb:', dataObj)
       } catch (error) {
         console.error('JSON parsing error:', error)
       }
@@ -226,62 +121,6 @@ const AdvancedChart = () => {
     setYesVisible(!yesVisible)
   }
 
-  // const colors = isAnomaly.map(() => {
-  //   if (isAnomaly == 1) {
-  //     return 'black'
-  //   } else return 'pink'
-  // })
-
-  // const showAnomaly = isAnomalyArr.map(() => {
-  //   if (isAnomalyArr == true) {
-  //     return index
-  //   }
-  //   else return
-  // })
-
-  // const highlightAnomaly = [
-  //   {
-  //     type: 'rect',
-  //     xref: 'x',
-  //     yref: 'paper',
-  //     x0: 400,
-  //     y0:2,
-  //     x1: 100,
-  //     y1: 1,
-      
-  //     fillcolor: 'red',
-  //     opacity: 0.2,
-  //     line: { width: 0 },
-  //   },
-    // {
-    //   type: 'rect',
-    //   xref: 'x',
-    //   yref: 'paper',
-    //   x1: 500,
-    //   y1: 1,
-    //   x0: 100,
-    //   y0: 0,
-      
-    //   fillcolor: 'red',
-    //   opacity: 0.2,
-    //   line: { width: 0 },
-    // },
-    // {
-    //   type: 'rect',
-    //   // x-reference is assigned to the x-values
-    //   xref: 'x',
-    //   // y-reference is assigned to the plot paper [0,1]
-    //   yref: 'paper',
-    //   x0: 1800,
-    //   y0: 0,
-    //   x1: 2100,
-    //   y1: 1,
-    //   fillcolor: 'red',
-    //   opacity: 0.2,
-    //   line: { width: 0 },
-    // },
-  //]
-
   const layout = {
     title: 'Anomaly Detection Plot',
     titlefont: { size: 20 },
@@ -302,19 +141,19 @@ const AdvancedChart = () => {
       side: 'right',
       zeroline: false,
     },
-    width: 1500,
+    width: 1900,
     height: 800,
     margin: {
       t: 50,
       b: 80,
       l: 100,
       r: 80,
-    // },
+    },
     // shapes: highlightAnomaly,
     responsive: true,
     useResizeHandler: true,
     autosize: true,
-  }}
+  }
 
   return (
     <Box
@@ -323,7 +162,8 @@ const AdvancedChart = () => {
     >
       <div>
         {/* <h2>WebSocket Line Chart Example</h2>  */}
-        <Plot data={testData} layout={layout} />
+        <Plot data={testData} layout={layout} useResizeHandler={true} responsive ={true} autosize={true}
+style={{width: '100%', height: '100%'}}   />
       </div>
       <Space direction="vertical">
         <Switch
