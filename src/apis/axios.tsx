34677c1825082ed4e12959d48b1b7b45c8c77ddc
Mira Jo <mira@Miraui-noteubug.local>
2024-05-10 10:06:32 +0200

    Sync axios baseURL with ENV variable

--- src/apis/axios.tsx
+++ src/apis/axios.tsx
@@ -1,19 +1,11 @@
 import axios from 'axios'
 
-const BASE_URL = 'https://ineeji-solution-tf.du.r.appspot.com'
-const DEV_SERVER_URL = 'http://34.64.98.129:8000'
-
 export const axiosPublic = axios.create({
-  baseURL: BASE_URL,
-  headers: { 'Content-Type': 'application/json' },
-})
-
-export const axiosPrivate = axios.create({
-  baseURL: DEV_SERVER_URL,
+  baseURL: process.env.REACT_APP_API_SERVER_URL,
   headers: { 'Content-Type': 'application/json' },
 })
 
 export const axiosProgress = axios.create({
-  baseURL: BASE_URL,
+  baseURL: process.env.REACT_APP_API_SERVER_URL,
   headers: { 'Content-Type': 'application/json' },
 })
