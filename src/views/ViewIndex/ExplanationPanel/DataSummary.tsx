7d6d87758c4f7c58c3a29ffd549b5edadadfd1fc
mira-ineeji <mira@ineeji.com>
2024-11-11 13:58:56 +0900

    feat: add symbol description and source to data summary

---
+++ src/views/ViewIndex/ExplanationPanel/DataSummary.tsx
@@ -0,0 +1,17 @@
+import { useRecoilValue } from 'recoil'
+import { SymbolListState, SymbolState } from '../stores/atom'
+
+const DataSummary = () => {
+  const symbolList = useRecoilValue(SymbolListState)
+  const symbol = useRecoilValue(SymbolState)
+
+  return (
+    <div className="m-3">
+      <h3 className="text-black text-lg">Data Summary</h3>
+      {symbolList?.filter((s) => s.symbol_id === symbol.symbol_id)[0]?.description} (출처 :
+      {symbolList?.filter((s) => s.symbol_id === symbol.symbol_id)[0]?.source})
+    </div>
+  )
+}
+
+export default DataSummary
