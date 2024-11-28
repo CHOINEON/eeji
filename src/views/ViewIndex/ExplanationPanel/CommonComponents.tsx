a849bcd8b4a76bcd42120a68f0056f98b03743f5
mira-ineeji <mira@ineeji.com>
2024-11-28 15:44:57 +0900

    chore: update styles

--- src/views/ViewIndex/ExplanationPanel/CommonComponents.tsx
+++ src/views/ViewIndex/ExplanationPanel/CommonComponents.tsx
@@ -1,3 +1,3 @@
 export const ComponentTitle = ({ title }: { title: string }) => {
-  return <p className="font-['Helvetica Neue'] text-[#002d65] text-lg font-bold">{title}</p>
+  return <span className="font-['Helvetica Neue'] text-[#002d65] text-lg font-bold">{title}</span>
 }
