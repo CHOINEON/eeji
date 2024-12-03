085f5431c58a8f2f632aec81f82c49b7f8d2b019
mira-ineeji <mira@ineeji.com>
2024-12-03 09:39:55 +0900

    feat: display 'no data' message when API returns 204 status

--- src/views/ViewIndex/VisualPanel/VisualPanel.tsx
+++ src/views/ViewIndex/VisualPanel/VisualPanel.tsx
@@ -1,7 +1,8 @@
-import { Spin } from 'antd'
+import { message, Spin } from 'antd'
 import IndexApi from 'apis/IndexApi'
 import { IPrediction } from 'apis/type/IndexResponse'
 import { useEffect, useState } from 'react'
+import { useTranslation } from 'react-i18next'
 import { useQuery } from 'react-query'
 import { useRecoilValue, useSetRecoilState } from 'recoil'
 import { capitalizeFirstLetter } from 'utils/StringFormatter'
@@ -11,6 +12,7 @@ import HorizonButtonGroup from './HorizonButtonGroup'
 import SymbolDropdown from './SymbolDropdown'
 
 const VisualPanel = () => {
+  const { t } = useTranslation()
   const symbol = useRecoilValue(SymbolState)
   const setGraphData = useSetRecoilState(graphDataState)
   const setRawData = useSetRecoilState(RawDataState)
@@ -26,7 +28,7 @@ const VisualPanel = () => {
     return IndexApi.getRawData(symbol.symbol_id)
   }
 
-  const { data: predictionData } = useQuery(
+  const { data: predictionData, isFetched: isPredictionFetched } = useQuery(
     ['predictionData', symbol.symbol_id, symbol.selectedHorizon],
     fetchPredictionData,
     {
@@ -42,7 +44,7 @@ const VisualPanel = () => {
     }
   )
 
-  const { data: rawData } = useQuery(['rawData', symbol.symbol_id], fetchRawData, {
+  const { data: rawData, isFetched: isRawFetched } = useQuery(['rawData', symbol.symbol_id], fetchRawData, {
     enabled: !!symbol.symbol_id,
     onSuccess: (data) => {
       if (data) setRawData(data)
@@ -52,8 +54,15 @@ const VisualPanel = () => {
   })
 
   useEffect(() => {
-    if (predictionData && rawData) setLoading(false)
-  }, [predictionData, rawData])
+    if (isPredictionFetched && isRawFetched) {
+      if (predictionData || rawData) {
+        setLoading(false)
+      } else {
+        setLoading(false)
+        message.info(t('No data'))
+      }
+    }
+  }, [isPredictionFetched, isRawFetched])
 
   return (
     <div className="m-3">
